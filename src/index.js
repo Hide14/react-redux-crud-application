9ddf98b78523f1b95b834b7e8ce629ff044010f1
Hide14 <hide14ryoumavoyage2@gmail.com>
2020-01-07 04:47:04 +0000

    implement store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+
+import App from './components/App';
 import './index.css';
+import reducer from './reducers'
+
+
+const store = createStore(reducer)
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+  <App />
+  </Provider>,
   document.getElementById('root')
 );
